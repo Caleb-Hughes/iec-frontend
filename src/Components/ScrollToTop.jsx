c6a87a44a935d3beacf5f57e8b9f4ecc82e49eb3
Caleb Hughes <Hughes.calebj@gmail.com>
2026-02-10 16:00:43 -0500

    Another attempt

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -4,13 +4,18 @@ import { useLocation } from 'react-router-dom';
 export default function ScrollToTop() {
     const { pathname } = useLocation();
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         window.scrollTo({
             top: 0,
             left: 0,
             behavior: "instant"
         });
+        document.documentElement.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "instant"
+        });
     }, [pathname]);
-    
+
     return null;
 }
